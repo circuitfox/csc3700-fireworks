988d677bb27e2a76ff8bfe33ec1b6ad62ad5ab33
Carl <carlgoshert@gmail.com>
2017-04-10 19:46:31 -0500

    Basic form of website layout

---
+++ resources/views/layouts/FireworksBase.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="fireworks shop admin dashboard page">
+    <meta name="author" content="Carl, Chris and Jameson">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <title>Fireworks Shop Admin Page</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a href="" class="navbar-brand">Fireworks Shop</a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="">Page 1</a></li>
+            <li><a href="">Page 2</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    @yield('content')
+  </body>
+</html>
