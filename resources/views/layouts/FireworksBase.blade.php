91c396d26dec17db7c5a75f76a10f3df44609d93
Carl <carlgoshert@gmail.com>
2017-04-10 20:10:42 -0500

    Added csrf meta tag

--- resources/views/layouts/FireworksBase.blade.php
+++ resources/views/layouts/FireworksBase.blade.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="description" content="fireworks shop admin dashboard page">
     <meta name="author" content="Carl, Chris and Jameson">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
