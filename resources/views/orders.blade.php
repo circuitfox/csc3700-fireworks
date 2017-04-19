0d04edc0aab3db9ab320c5eda457e67086b61a4c
Chris Stankus <cdstankus@gmail.com>
2017-04-19 12:18:38 -0500

    Change old layout name

--- resources/views/orders.blade.php
+++ resources/views/orders.blade.php
@@ -1,4 +1,4 @@
-@extends("layouts/FireworksBase")
+@extends("layouts/app")
 @section("content")
 <div class="container col-md-8 col-md-offset-2">
     @include("partials.orders", ["orders" => $orders])
