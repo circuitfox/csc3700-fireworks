777fa57914dc8df0f3b095c65ce2ff973334c4b3
Carl <carlgoshert@gmail.com>
2017-04-26 12:09:38 -0500

    changed is_admin default to 0

--- database/migrations/2017_04_09_001233_users-add-admin-column.php
+++ database/migrations/2017_04_09_001233_users-add-admin-column.php
@@ -14,7 +14,7 @@ class UsersAddAdminColumn extends Migration
   public function up()
   {
     Schema::table("users", function (Blueprint $table) {
-      $table->boolean("is_admin")->default(NULL);
+      $table->boolean("is_admin")->default(0);
     });
   }
 
