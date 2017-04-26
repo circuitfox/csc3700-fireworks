0cb949cd30b4bea8586ee1edcea9de96c07440b5
Chris Stankus <cdstankus@gmail.com>
2017-04-26 08:59:44 -0500

    Dashboard page tests

--- tests/Feature/WelcomePageTest.php
+++ tests/Feature/WelcomePageTest.php
@@ -31,4 +31,42 @@ class WelcomePageTest extends TestCase
             ->assertSee("tab-content")
             ->assertSee("tab-pane");
     }
+
+    public function testHasInsertButton()
+    {
+        $this->get($this->getRoute())
+            ->assertSee("/products/create")
+            ->assertSee("Add Product");
+    }
+
+    public function testHasProducts()
+    {
+        $products = factory(\App\Product::class, 10)->create();
+        $page = $this->get($this->getRoute());
+        foreach ($products as $product) {
+            print($product->id . "\n");
+            $page->assertSee("product" . $product->id)
+                ->assertSee($product->description)
+                ->assertSee("edit-product" . $product->id)
+                ->assertSee("/products/" . $product->id . "/edit")
+                ->assertSee($product->brand);
+        }
+        $page->assertSee("product-delete-modal");
+    }
+
+    public function testHasOrders()
+    {
+        $user = factory(\App\User::class)->create();
+        $orders = factory(\App\Order::class, 10)->create()->each(function($o) use ($user) {
+            $o->user()->associate($user);
+            $o->save();
+        });
+        $page = $this->get($this->getRoute());
+        foreach ($orders as $order) {
+            $page->assertSee("#order" . $order->id)
+                ->assertSee("Order " . $order->id)
+                ->assertSee("for " . htmlspecialchars($order->user->name, ENT_QUOTES))
+                ->assertSee("for " . htmlspecialchars($user->name, ENT_QUOTES));
+        }
+    }
 }
