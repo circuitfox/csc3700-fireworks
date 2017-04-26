c79a1228af41cb79ef92c114213f4e8ef88a6ceb
Carl <carlgoshert@gmail.com>
2017-04-26 13:42:07 -0500

    bug fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,12 +3,8 @@
 @if(Auth::user()->is_admin == 1)
   <div class="container">
     <ul class="nav nav-tabs col-md-offset-1 col-md-9">
-      <li class="active">
-        <a href="#tab1" data-toggle="tab">Products</a>
-      </li>
-      <li>
-        <a href="#tab2" data-taggle="tab">Orders</a>
-      </li>
+      <li class="active"><a href="#tab1" data-toggle="tab">Products</a></li>
+      <li><a href="#tab2" data-toggle="tab">Orders</a></li>
     </ul>
     <div class="tab-content col-md-offset-1 col-md-9" id="TabContents">
       <div class="tab-pane fade active in" id="tab1">
