c29038b4a3748f24d5a1c845725e529749b938fb
Chris Stankus <cdstankus@gmail.com>
2017-04-26 21:45:38 -0500

    Order body and testing seeder

--- resources/views/partials/order/body.blade.php
+++ resources/views/partials/order/body.blade.php
@@ -1,1 +1,36 @@
-<!-- FIXME: Product orders -->
+<div class="container">
+  <div class="col-sm-8">
+    <table class="table">
+      <tr>
+        <th>Quantity</th>
+        <th>Product</th>
+        <th>Price</th>
+      </tr>
+    @foreach ($order->productOrders as $productOrder)
+      <tr>
+        <td>{{ $productOrder->quantity }}</td>
+        <td>
+          <b><a href="/products/{{ $productOrder->product->id }}">
+            {{ $productOrder->product->description }}
+          </a></b>
+        </td>
+        <td>
+          ${{ $productOrder->quantity * $productOrder->product->piece_price }}
+        </td>
+      </tr>
+    @endforeach
+      <tfoot>
+        <tr>
+          <td><b>Total Price:</b></td>
+          <td></td>
+          <td>
+            ${{ $order->productOrders->reduce(function ($acc, $po) {
+                    return $acc + $po->product->piece_price;
+                })
+             }}
+          </td>
+        </tr>
+      </tfoot>
+    </table>
+  </div>
+</div>
