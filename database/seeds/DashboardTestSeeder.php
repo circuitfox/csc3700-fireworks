4d2e63d1e1fb78bb25a92f514212762799bf2ef6
Carl <carlgoshert@gmail.com>
2017-04-29 23:29:44 -0500

    fixed indenting

--- database/seeds/DashboardTestSeeder.php
+++ database/seeds/DashboardTestSeeder.php
@@ -11,19 +11,19 @@ class DashboardTestSeeder extends Seeder
      */
     public function run()
     {
-      $user = factory(\App\User::class)->create();
-      $orders = factory(\App\Order::class, 5)->create()->each(function ($o) use ($user) {
+        $user = factory(\App\User::class)->create();
+        $orders = factory(\App\Order::class, 5)->create()->each(function ($o) use ($user) {
             $o->user()->associate($user);
             $o->save();
         });
         foreach ($orders as $order) {
             factory(\App\ProductOrder::class, 5)
-                ->states("with_product")
-                ->create()
-                ->each(function ($po) use ($order) {
-                    $po->order()->associate($order);
-                    $po->save();
-                });
+            ->states("with_product")
+            ->create()
+            ->each(function ($po) use ($order) {
+                $po->order()->associate($order);
+                $po->save();
+            });
         }
     }
 }
