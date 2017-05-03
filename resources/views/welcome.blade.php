103ae61098719013bcfb4241001bc8ecb1818ffc
Chris Stankus <cdstankus@gmail.com>
2017-05-03 09:06:26 -0500

    fix quote error in routes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,8 @@
     <h1>Welcome to the website!</h1>
     <p>Please log in or register</p>
     <div class="row">
-      <a href="{{ route('login) }}" class="btn btn-primary col-md-2 col-md-offset-4">Login</a>
-      <a href="{{ route('register) }}" class="btn btn-primary col-md-2 col-md-offset-1">Register</a>
+      <a href="{{ route('login') }}" class="btn btn-primary col-md-2 col-md-offset-4">Login</a>
+      <a href="{{ route('register') }}" class="btn btn-primary col-md-2 col-md-offset-1">Register</a>
     </div>
   </div>
 </div>
